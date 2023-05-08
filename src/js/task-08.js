be2ff3cb506c49a2b9578f0921911b5d74330b41
Sergii Marchenko <marikmarchelo@gmail.com>
2023-05-08 10:47:34 +0300

    task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (!emailInput.value || !passwordInput.value) {
+    alert("Всі поля обов'язкові для заповнення!");
+    return;
+  }
+
+  const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+});
