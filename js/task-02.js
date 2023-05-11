109ae39dcc376785cb4a9f013cbdcb6b4ac2dbdc
Sergii Marchenko <marikmarchelo@gmail.com>
2023-05-11 20:15:04 +0300

    Виправив помилки

--- js/task-02.js
+++ js/task-02.js
@@ -8,9 +8,15 @@ const ingredients = [
 ];
 
 const ul = document.getElementById("ingredients");
+const liElements = [];
+
 ingredients.forEach((ingredient) => {
   const li = document.createElement("li");
   li.textContent = ingredient;
   li.classList.add("item");
+  liElements.push(li);
+});
+
+liElements.forEach((li) => {
   ul.appendChild(li);
 });
--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,5 @@
 const inputRef = document.querySelector("#font-size-control");
-console.log(inputRef);
 const spanRef = document.querySelector("#text");
-console.log(spanRef);
 
 inputRef.addEventListener("input", () => {
   spanRef.style.fontSize = inputRef.value + "px";
